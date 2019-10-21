8a761170fc34c69c997618f23800c59e8a2ac758
Damelya <n.damelya.s@gmail.com>
2019-10-21 16:21:05 +0600

    hj

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -3,6 +3,8 @@ package piscine
 func RecursiveFactorial(n int) int {
 	if n == 0 {
 		return 1
+	}else if n<2000000{
+		return n * RecursiveFactorial(n-1)
 	}
-	return n * RecursiveFactorial(n-1)
+	return 0
 }
